fbefa7ad866782d4b9dd6d48e0a928549344c899
arek <jack-swad@wp.pl>
2022-02-06 14:32:45 +0000

    added flipping animation, stop on final date,  prevented -1 values

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 import './scss/style.scss';
 import '../index.html';
-// js modules
 
-const x = 'a';
+// js modules
+import Countdown from './js/countdown';
+
+// =====================================================================
+// =====================    COUNTDOWN     ==============================
+// =====================================================================
+
+// containers
+const daysContainer = document.querySelector(`[data-days]`);
+const hoursContainer = document.querySelector(`[data-hours]`);
+const minutesContainer = document.querySelector(`[data-minutes]`);
+const secondsContainer = document.querySelector(`[data-seconds]`);
+
+// getTime turns date into miliseconds since 1 january 1970 00:00:00
+const finalDate = new Date(`April 17, 2022 00:00:00`).getTime();
+
+const countdown = new Countdown(daysContainer, hoursContainer, minutesContainer, secondsContainer, finalDate);
+
+// Just run damn clock
+countdown.runClock();
---
+++ src/js/countdown.js
@@ -0,0 +1,112 @@
+class Countdown {
+  constructor(daysContainer, hoursContainer, minutesContainer, secondsContainer, finalDate) {
+    // Container
+    this.daysContainer = daysContainer;
+    this.hoursContainer = hoursContainer;
+    this.minutesContainer = minutesContainer;
+    this.secondsContainer = secondsContainer;
+    // Final date
+    this.finalDate = finalDate;
+    // First update flag
+    this.flag = false;
+    // Prev values
+    this.prevSec = ``;
+    this.prevMin = ``;
+    this.prevHour = ``;
+    this.prevDay = ``;
+  }
+
+  runClock = () => {
+    // Get current date and compute time gap
+    const currentDate = new Date().getTime();
+    const timeGap = this.finalDate - currentDate;
+    // Stop when timer shows 0:0:0:0
+    if (timeGap >= 0) {
+      // Set time units
+      const now = {
+        sec: Math.floor((timeGap / 1000) % 60),
+        min: Math.floor((timeGap / (1000 * 60)) % 60),
+        hour: Math.floor((timeGap / (1000 * 60 * 60)) % 24),
+        day: Math.floor(timeGap / (1000 * 60 * 60 * 24)),
+      };
+
+      this.checkIfFlipNeeden(now);
+      this.updatePrevValues(now);
+
+      // Recurency, invoke yourself after 1sec
+      setTimeout(() => {
+        this.runClock();
+      }, 1000);
+    }
+  };
+
+  // If unit is not the same as before, flip it
+  checkIfFlipNeeden = ({ sec, min, hour, day }) => {
+    if (this.prevDay !== day) this.flip(this.daysContainer, day);
+    if (this.prevHour !== hour) this.flip(this.hoursContainer, hour);
+    if (this.prevMin !== min) this.flip(this.minutesContainer, min);
+    if (this.prevSec !== sec) this.flip(this.secondsContainer, sec);
+  };
+
+  // We need prev values to check if unit has changed
+  updatePrevValues = ({ sec, min, hour, day }) => {
+    this.prevSec = sec;
+    this.prevMin = min;
+    this.prevHour = hour;
+    this.prevDay = day;
+  };
+
+  // Freez function for X ms because we want to remove flip cards after they flip
+  sleep = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  flip = (card, currNumber) => {
+    // console.log(currNumber)
+    const nextNumber = this.getNextNumber(card, currNumber);
+    // console.log(nextNumber)
+    card.querySelector(`span.timer__flip-front`).classList.add(`flipped`); // Front do pionu
+    card.querySelector(`span.timer__flip-back`).classList.remove(`flipped`); // Back na dół
+    this.sleep(300).then(() => {
+      this.removeFlippedCards(card); // Skasuj karty
+      card.setAttribute(`data-number-before`, nextNumber); // Before
+      card.setAttribute(`data-number-after`, currNumber); // After
+      this.createNewFlipCards(card, currNumber, nextNumber); // Zrób karty
+    });
+  };
+
+  getNextNumber = (card, currNumber) => {
+    if (`days` in card.dataset) {
+      // Update day
+      --currNumber < 0 ? (currNumber = 0) : ``;
+    } else if (`hours` in card.dataset) {
+      // Update hour
+      --currNumber < 0 ? (currNumber = 23) : ``;
+    } else if (`minutes` in card.dataset || `seconds` in card.dataset) {
+      // Update min / sec
+      --currNumber < 0 ? (currNumber = 59) : ``;
+    }
+    return currNumber;
+  };
+
+  removeFlippedCards = (card) => {
+    // Remove flipped cards
+    card.querySelector(`span.timer__flip-front`).remove();
+    card.querySelector(`span.timer__flip-back`).remove();
+  };
+
+  createNewFlipCards = (card, currNumber, nextNumber) => {
+    // Create front card
+    const newFront = document.createElement(`span`);
+    newFront.classList.add(`timer__flip-front`);
+    newFront.textContent = currNumber;
+    // Create back card
+    const newBack = document.createElement(`span`);
+    newBack.classList.add(`timer__flip-back`, `flipped`);
+    newBack.textContent = nextNumber;
+    // Append Cards
+    card.append(newFront, newBack);
+  };
+}
+
+export default Countdown;
